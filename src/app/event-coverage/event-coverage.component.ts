8a4e8578bfbf47aa0e8bb9be9862f8112a1ddcf7
mdjeghbala <mohamed.djeghbala@outlook.fr>
2025-07-11 16:30:56 +0200

    Update display and retrieve product prices

--- src/app/event-coverage/event-coverage.component.ts
+++ src/app/event-coverage/event-coverage.component.ts
@@ -353,6 +353,7 @@ export class EventCoverageComponent {
   }
 
   GARANTIE_CODES = {
+    RC : 1,
     DEFENSE_RECOURS: 350,
     ANNULATION: 351,
     INTEMPERIES: 352,
@@ -360,6 +361,20 @@ export class EventCoverageComponent {
   };
 
   garantiePrices: { [key: string]: number } = {};
+
+  calculateIAIPrices() {
+    const amount = this.coverageOptionsForm.get('reservationAmount')?.value;
+    if (amount && amount > 0) {
+      ['INTEMPERIES', 'ANNULATION', 'INTERRUPTION'].forEach(garantie => {
+        this.calculateGarantiePrice(this.GARANTIE_CODES[garantie as keyof typeof this.GARANTIE_CODES]);
+      });
+    } else {
+      ['INTEMPERIES', 'ANNULATION', 'INTERRUPTION'].forEach(garantie => {
+        delete this.garantiePrices[garantie];
+      });
+      this.garantiePrices = { ...this.garantiePrices }; 
+    }
+  }
 
   goToNextStep(): void {
     if (this.isMinor()) {
@@ -370,17 +385,13 @@ export class EventCoverageComponent {
   }
 
   goToCoverageChoicesStep(): void {
-    console.log("goToCoverageChoicesStep");
-    Object.keys(this.GARANTIE_CODES).forEach(key => console.log(key));
-    Object.keys(this.GARANTIE_CODES).forEach((garantie : string) => {
-      console.log(garantie);
+    ['RC', 'DEFENSE_RECOURS'].forEach((garantie : string) => {
       this.calculateGarantiePrice(this.GARANTIE_CODES[garantie as keyof typeof this.GARANTIE_CODES]);
     })
     this.stepper.next();
   }
 
   goToEventOrMotorsLeaguePage(): void {
-    console.log("goToEventOrMotorsLeaguePage");
     this.router.navigate(['/guarantee-choice']);
   }
 
