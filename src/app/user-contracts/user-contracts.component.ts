e86d244493195c37afec9726a8120fae0c0f8783
mdjeghbala <mohamed.djeghbala@outlook.fr>
2025-09-24 13:34:01 +0200

    Update icons color according to valid or invalid contract

--- src/app/user-contracts/user-contracts.component.ts
+++ src/app/user-contracts/user-contracts.component.ts
@@ -52,7 +52,6 @@ export class UserContractsComponent implements AfterViewInit {
     'adherentnomCliententreprise', 
     'products', 
     'circuit', 
-    'valide',
   ];
   
   mobileColumns: string[] = [
